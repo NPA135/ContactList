81ab23acec6cdc5b311f6ae5feda46a230cab3a4
NPA135 <noahpoea@gmail.com>
2024-10-03 13:36:34 -0700

    added 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
-  
-  const [contacts, setContacts] = useState(dummyContacts)
-  console.log("Contacts: ", contacts)
-  
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList/>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   );
 }
--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -6,8 +6,24 @@ const dummyContacts = [
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
+
 export default function ContactList() {
+
   const [contacts, setContacts] = useState(dummyContacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
 
   return (
     <table>
